d0e655a6eaf7767146ff551c204aeaabad26fb10
huangxg <huang.xingang@gmail.com>
2022-04-24 15:40:00 -0700

    add Vue compoents concert, video, program

---
+++ concerts.js
@@ -0,0 +1,35 @@
+const echo = {
+  title: 'The Echo',
+  datetime: 'Sunday, Dec 5, 2021 6:30 pm',
+  venue: [ 'Forum Theatre, Yorba Linda High School',
+           '4175 Fairmont Blvd, Yorba Linda, CA 92886' ],
+  poster: 'img/echo.jpg',
+  video: 'https://www.youtube.com/embed/98-eAkatJSY',
+  program: [
+    [ 'River Flows in You', 'Yiruma', 'Tina Hou, piano' ],
+    [ 'Safe and Sound', 'Swift, Williams, White, and Burnett', 'Samantha Huang & Nadia Wang, soprano · Evelyn Wu, flute · Cathy Chen, guitar' ],
+    [ 'Octet', 'Schubert; arr: Brown', 'Tina Hou & Cathy, piano' ],
+    [ 'Carnivale', 'Roubos', 'Tina Hou & Cathy, piano' ],
+    [ 'Canon in D', 'Pachelbel', 'Evelyn, flute · Ievtte Chen, cello · Vella Feng, piano' ],
+    [ 'Rhythm in Northern Country', 'Walker', 'Cathy Chen, piano' ],
+    [ 'What a Wonderful World', 'Thiele and Weiss', 'Samantha Huang & Nadia Wang, soprano · Evelyn Wu, flute · Vella Feng, piano' ],
+    [ 'Impromptu', 'Reinhold', 'Anna Huang, piano' ],
+    [ 'A Thousand Years', 'Perri and Hodges', 'Ivette Chen, cello · Cathy Chen, piano' ],
+    [ 'Unravel Tokyo Ghoul', 'Kitajima', 'Vella Feng, piano' ],
+    [ "Fisherman's Song at Eventide", 'Young', 'Evelyn Wu, flute · Anna Huang, piano' ],
+    [ "Can't Help Fall in Love", 'Peretti, Creatore, and Weiss', 'Samantha Huang & Nadia Wang, soprano · Ivette Chen, cello · Cathy Chen, guitar · Tina Hou, piano' ],
+    [ 'Concert Etude No. 3 Un Sospiro', 'Liszt', 'Nadia Wang, piano' ],
+    [ 'Pezzo Cariccioso', 'Tchaikovsky', 'Ivette Chen, cello · Anna Huang, piano' ],
+    [ 'Starlight', 'Bellamy; arr: Chow', 'Samantha Huang & Nadia Wang, soprano · Evelyn Wu & Anna Huang, flute · Ivette Chen, cello · Cathy Chen, guitar · Vella Feng & Tina Hou, piano' ],
+  ]
+};
+
+const dawn = {
+  title: 'The Dawn',
+  datetime: 'Sunday, May 22, 2022 6:00 pm',
+  venue: [ 'Bronwyn Dodson Theatre, Fullerton College',
+           '321 E Chapman Ave, Fullerton, CA 92832' ],
+  poster: 'img/dawn.jpg',
+};
+
+export { dawn, echo };
---
+++ util.js
@@ -0,0 +1,40 @@
+function initVue() {
+  Vue.component('concert-box', {
+    props: ['concert'],
+    template: '<div class="concert-box">' +
+                '<h2>{{ concert.title }}</h2>' +
+                '<table class="concert-table">' +
+                '<tr>' + 
+                '  <th>Date and Time</th>' +
+                '  <td>{{ concert.datetime }}</td>' +
+                '</tr><tr>' +
+                '  <th>Venue</th>' +
+                '  <td>{{ concert.venue[0] }}<br>{{ concert.venue[1] }}</td>' +
+                '</tr>' +
+                '</table>' +
+                '<img class="concert-poster figure-img img-fluid rounded mb-5" :src="concert.poster">' +
+                '<slot></slot>' +
+              '</div>'
+  });
+
+  Vue.component('concert-video', {
+    props: ['url'],
+    template: '<div class="video-wrap mb-5"><div class="video-container">' +
+                '<iframe :src="url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
+              '</div></div>'
+  });
+
+  Vue.component('concert-program', {
+    props: ['program'],
+    template: '<div class="concert-program">' +
+                '<h3>Concert Program</h3>' +
+                '<v-card v-for="(item,i) in program" :key="i" class="mx-auto my-3">' +
+                  '<v-card-title>{{item[0]}}</v-card-title>' +
+                  '<v-card-subtitle>composer(s): {{item[1]}}</v-card-subtitle>' +
+                  '<v-card-text>performer(s): {{item[2]}}</v-card-text>' +
+                '</v-card>' +
+              '</div>'
+  });
+}
+
+export { initVue };
